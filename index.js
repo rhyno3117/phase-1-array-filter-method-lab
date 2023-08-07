db1f12b412574be24106f401caed8ca987b1fbec
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-07 13:15:17 -0600

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter
+        (possibleMatch => possibleMatch.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(
+        (possibleMatch) =>
+            possibleMatch.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((record) => record.name === name)
+}
